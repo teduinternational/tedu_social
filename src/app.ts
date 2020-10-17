4e538b2fff173a3bc465c9f4d71ff47485f61bda
Toan Bach <tedu.international@gmail.com>
2020-10-17 15:03:49 +0700

    connect to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
+import mongoose from 'mongoose';
 
 class App {
   public app: express.Application;
@@ -10,6 +11,7 @@ class App {
     this.port = process.env.PORT || 5000;
 
     this.initializeRoutes(routes);
+    this.connectToDatabase();
   }
 
   public listen() {
@@ -22,6 +24,22 @@ class App {
     routes.forEach((route) => {
       this.app.use('/', route.router);
     });
+  }
+
+  private connectToDatabase() {
+    try {
+      const connectString =
+        'mongodb+srv://tedu:LLrvB941gFsxcQmz@master.0rrty.mongodb.net/tedu_social?retryWrites=true&w=majority';
+      mongoose.connect(connectString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      });
+      console.log('Database connected...');
+    } catch (error) {
+      console.log('Connect to database error');
+    }
   }
 }
 
