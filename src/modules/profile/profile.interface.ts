0a13d94464d8ae7c8fc0e4701669e65542a5045c
Toan Bach <tedu.international@gmail.com>
2020-11-30 07:25:53 +0700

    Create profile model

---
+++ src/modules/profile/profile.interface.ts
@@ -0,0 +1,42 @@
+export interface IProfile {
+  _id: string;
+  user: string;
+  company: string;
+  website: string;
+  location: string;
+  status: string;
+  skills: string[];
+  bio: string;
+  experience: IExperience[];
+  education: IEducation[];
+  social: ISocial;
+  date: Date;
+}
+
+export interface IExperience {
+  _id: string;
+  title: string;
+  company: string;
+  location: string;
+  from: Date;
+  to: Date;
+  current: boolean;
+  description: string;
+}
+export interface IEducation {
+  _id: string;
+  school: string;
+  degree: string;
+  fieldofstudy: string;
+  from: Date;
+  to: Date;
+  current: boolean;
+  description: string;
+}
+export interface ISocial {
+  youtube: string;
+  twitter: string;
+  linkedin: string;
+  facebook: string;
+  instagram: string;
+}
