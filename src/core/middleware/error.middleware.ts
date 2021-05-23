a07a74116d4272afc20028ffd81e961c39b22c51
Toan Bach <tedu.international@gmail.com>
2021-05-23 09:06:54 +0700

    Add logging

--- src/core/middleware/error.middleware.ts
+++ src/core/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
+import HerokuLogger from 'heroku-logger';
 import { HttpException } from '@core/exceptions';
 import { Logger } from '@core/utils';
 
@@ -8,6 +9,7 @@ const errorMiddleware = (error: HttpException, req: Request, res: Response, next
   const message: string = error.message || 'Some thing when wrong';
 
   Logger.error(`[ERROR] - Status: ${status} - Msg: ${message}`);
+  HerokuLogger.error(`[ERROR] - Status: ${status} - Msg: ${message}`);
   res.status(status).json({ message: message });
 };
 
